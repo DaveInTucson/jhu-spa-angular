0285fcfe6138e04bc22a4472ec9c5f7adc26ddc1
Dave in Tucson <dave.in.tucson.az@gmail.com>
2018-01-08 00:55:54 -0700

    implemented narrow it down button

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -13,21 +13,28 @@
   {
     let nidc = this;
     nidc.error = null;
+    nidc.matchedItems = null;
     // console.log('in NarrowItDownController');
-    MenuSearchService.getAllMenuItems().then(function (response)
-      {
-        nidc.allMenuItems = response.data;
-        // console.log('allMenuItems=', nidc.allMenuItems);
-      }
-    ).catch(function(response)
-      {
-        ndic.error = "Error fetching menu items: " + response;
-      }
-    );
+
+    nidc.OnNarrowItDown = function()
+    {
+        let nidc = this;
+        MenuSearchService.getMatchedMenuItems(nidc.searchTerm)
+          .then(function (response)
+          {
+            // console.log('response=', response);
+            nidc.matchedItems = response;
+          })
+          .catch(function (response)
+          {
+            console.log('error: ', response);
+            nidc.error = "Error fetching menu items: " + response;
+          });
+    };
   };
 
-  MenuSearchService.$inject = ['$http'];
-  function MenuSearchService($http)
+  MenuSearchService.$inject = ['$http', '$q'];
+  function MenuSearchService($http, $q)
   {
     let service = this;
 
@@ -39,7 +46,40 @@
         method: 'GET',
         url: "https://davids-restaurant.herokuapp.com/menu_items.json"
       };
-      return $http(o);
+      let promise = $http(o);
+      promise.then(function(response)
+      {
+        service.allMenuItems = response.data;
+      });
+      return promise;
+    };
+
+    function filterMenuItems(menuItems, searchTerm)
+    {
+      let filteredItems = [];
+      // console.log('have ', menuItems.length, ' items, filtering on ', searchTerm)
+      for (let i = 0; i < menuItems.length; i++)
+      {
+        if (menuItems[i].description.indexOf(searchTerm) !== -1)
+        {
+          filteredItems.push(menuItems[i]);
+        }
+      }
+      // console.log('matched ', filteredItems.length, ' items');
+      return filteredItems;
+    };
+
+    service.getMatchedMenuItems = function(searchTerm)
+    {
+      return service.getAllMenuItems()
+        .then(function(response)
+        {
+          let matchedMenuItems = filterMenuItems(response.data.menu_items, searchTerm);
+          let deferred = $q.defer();
+          deferred.resolve(matchedMenuItems);
+          return deferred.promise;
+        });
+
     };
   };
 
